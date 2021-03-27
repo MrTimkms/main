a25d9288a50d922091efe35c0403fa71a502b240
Тимофеев Георий Андреевич <timofeevg3@gmail.com>
2021-03-27 10:46:48 +1000

    Закончил авторизацию, начало работы над контентом Signed-off-by: Тимофеев Георий Андреевич <timofeevg3@gmail.com>

--- scripts/autorization.js
+++ scripts/autorization.js
@@ -1,37 +1,34 @@
-/* Авторизация */
-$("#buttongetp").click(function () {
+$("#nopass").hide();
+$("#yespass").hide();
+$("#avtdiv").hide();
+//При авторизации
+$(function() {
+  $(document).on("click", "#avtbtn", function() {
+    $("#nopass").alert();
     var login = $("#inputlogin").val();
     var pass = $("#inputPassword").val();
     var login1 = "admin";
     var login2 = "Admin";
-    if (pass == login1 & login == login1 && login == login2) {
-        buttonGetPass.hidden = true,
-            buttonSignin.hidden = false,
-            buttonback.hidden = false,
-            textboxmail.hidden = true;
-        TextBoxPass.style.visibility = 'visible';
-        if ($.cookie("passIsSend") == "true") {
-            $("#info").text("Временный пароль уже сгенерирован и отправлен. Пожалуйста, воспользуйтесь им!");
-        }
-        else {
-            var avtListjson = {
-                nameProc: "getPass",
-                login: $("#login").val().trim(),
-            };
-            let json = JSON.stringify(avtListjson);
-            $.ajax({
-                url: "AppAdress/hs/main/proc",
-                type: "POST",
-                data: json,
-                dataType: "html",
-                beforeSend: funcBefore("getPass"),
-                success: funcSu
-            });
-        }
+    if (pass == login1 && login == login1) {
+      $("#yespass").show();
+      setTimeout(function() {
+        $("#avtdiv").hide();
+      }, 2000);
+    } else {
+      $("#nopass").show();
+      window.setTimeout(function() {
+        $("#nopass").hide();
+      }, 5000);
     }
-    else {
-        $("#login").notify("E-mail введён не корректно", "error");
-    }
+  });
 });
-
-window.onload = function () { TextBoxPass.style.visibility = 'hidden'; }
+$(function() {
+  $(document).on("click", "#Vertikalmenu", function() {
+    var nopas = $("#Vertikalmenu").prop("checked");
+    if (nopas == true) {
+      $("#indexbutn").addClass("btn-group-vertical");
+    } else {
+      $("#indexbutn").removeClass("btn-group-vertical");
+    }
+  });
+});
